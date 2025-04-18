e3bfc5f7b7cef0813df8fbcff09edb865e42e4ab
baskar <baskarwebtech@gmail.com>
2025-04-18 21:08:13 +0530

    first commit

---
+++ client/src/app/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+const Login = () => {
+  return (
+   <div className='w-full z-10 bg-white fixed top-0 left-0'>
+        <div className="h-40 max-w-xl mx-auto  shadow-2xl flex items-center justify-center">
+            Login
+        </div>
+   </div>
+  )
+}
+
+export default Login
